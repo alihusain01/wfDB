ad4ee2e9f3df640916c6b3eb1dafad1ce71586d7
Bgo318 <maldude18@gmail.com>
2020-01-25 17:02:38 -0600

    nice

---
+++ registration.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    // Get the checkbox
+    var radio = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+
+    // If the checkbox is checked, display the output text
+    if (radio.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var radio = document.getElementById("myCheck2");
+
+    var text = document.getElementById("text");
+
+    if ( radio.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+
+}
